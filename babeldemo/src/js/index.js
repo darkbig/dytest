411605bd02d4c4356805d0da90ec17c82c08b157
du86796922 <du86796922@users.noreply.github.com>
2017-06-27 15:59:48 +0800

    生产文件分离

--- babeldemo/src/js/index.js
+++ babeldemo/src/js/index.js
@@ -1,14 +1,17 @@
 /**
  * Created by dy on 2017/6/22.
  */
-require('../../node_modules/jQuery/tmp/jquery.js');
+var $=require('jquery');
 
 import css from'../css/index.css';
 
 import less from '../css/divbg.less';
+
+import json from '../../data/test.json';
+
 define('aaa',function(){
-    return '妹妹';
+    return '帅哥';
 });
 
 $('#wel').html(
-    '欢迎————————'+require('aaa'));
+    '欢迎————————'+require('aaa')+json.name);
--- babeldemo/webpack.config.js
+++ babeldemo/webpack.config.js
@@ -2,6 +2,7 @@
  * Created by dy on 2017/6/21.
  */
 var webpack = require('webpack');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');//引入插件
 var path = require('path');
 module.exports = {
     entry: __dirname + '/src/js/index.js',
@@ -27,24 +28,37 @@ module.exports = {
                 }
             }
         }, {
+            /*test: /\.css$/,
+             include: path.resolve(__dirname, './src/'),
+             exclude: path.resolve(__dirname, './node_modules/'),
+             use: [
+             {loader: "style-loader"},
+             {loader: "css-loader"}
+             ]*/
             test: /\.css$/,
-            include: path.resolve(__dirname, './src/'),
-            exclude: path.resolve(__dirname, './node_modules/'),
-            use: [
-                {loader: "style-loader"},
-                {loader: "css-loader"}
-            ]
-        },{
+            use: ExtractTextPlugin.extract({
+                fallback: "style-loader",
+                use: "css-loader"
+            })//参数为将css处理的loader
+        }, {
             test: /\.less$/,
             include: path.resolve(__dirname, './src/'),
             exclude: path.resolve(__dirname, './node_modules/'),
             use: [{
-                loader: "less-loader" // compiles Less to CSS
-            },{
                 loader: "style-loader" // creates style nodes from JS strings
             }, {
                 loader: "css-loader" // translates CSS into CommonJS
+            }, {
+                loader: "less-loader" // compiles Less to CSS
             }]
+        }, {
+            test: /\.json$/,
+            include: path.resolve(__dirname, './src/'),
+            exclude: path.resolve(__dirname, './node_modules/'),
+            use: 'json-loader'
         }]
-    }
+    }, plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        new ExtractTextPlugin("style.css")//意思是如果在index.js文件中看到css文件，编译完成后将它单独提出封装并命名为style.css
+    ]
 };
